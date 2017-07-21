edeca0e3f00ec6f5f8c9470a8f0d0758e93206b7
Robyn Kwok <jingwenkwok@gmail.com>
2017-07-21 13:27:45 +0800

    added sign up form

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,9 @@
+const userReducer = (state = [], action) => {
+
+  switch (action.type) {
+    default:
+        return state;
+  }
+}
+
+export default userReducer;
---
+++ src/store/store.js
@@ -0,0 +1,23 @@
+// Import Redux dependencies
+import { createStore, compose, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+// Import reducers'
+import userReducer from '../reducers/userReducer';
+
+export let initStore = () => {
+
+  // Combine reducers
+  const reducer = combineReducers({
+    users: userReducer
+  });
+
+  // Create the store with all the reducers and allow for chrome redux dev tools to run and read reducers
+  const store = createStore(reducer, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  ));
+
+  return store;
+
+}
