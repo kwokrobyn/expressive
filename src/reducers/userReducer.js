fe74841607cd6fb6111ba21ed96758cfbfb9ac7c
Robyn Kwok <jingwenkwok@gmail.com>
2017-07-22 11:05:04 +0800

    error messages included in state (no render)

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -4,21 +4,33 @@ const defaultUser = {
   uid: uuid.v4(),
   email: "",
   displayName: "Guest",
-  isSignedIn: false
+  isSignedIn: false,
+  hasAuthError: false,
+  errorMessage: ""
 }
 
 const userReducer = (state = defaultUser, action) => {
 
   switch (action.type) {
     case "SIGN_IN_SUCCESS":
+    // uid, email, displayName currently pulled from default firebase.auth() db.  
       return {
         uid: action.user.uid,
         email: action.user.email,
         displayName: action.user.displayName,
-        isSignedIn: true
+        isSignedIn: true,
+        hasAuthError: false,
+        errorMessage: ""
       }
     case "SIGN_OUT":
       return defaultUser;
+    // if auth error is true, update errorMessage
+    case "AUTH_ERROR":
+      return {
+        ...state,
+        hasAuthError: true,
+        errorMessage: action.error
+      }
     default:
         return state;
   }
