b6bdff5715c3bd908572c1fa45cbda18125c171c
woodysee <woodyseesk@gmail.com>
2017-07-24 11:34:21 +0800

    Added basic navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -23,12 +23,31 @@ class Navbar extends Component {
  render() {
    return (
     <div className="container">
-      <div className=""></div>
-      <div className="row">
-        <h2 className="col-sm-8">Navbar</h2>
-        <Link to="/signup" className="col-sm-2 navlink">Signup</Link>
-        <Link to="/login" className="col-sm-2 navlink">Login</Link>
-      </div>{ /* row */ }
+      <nav className="navbar navbar-inverse navbar-fixed-top"     role="navigation">
+        <div className="container">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+              <span className="sr-only">Toggle navigation</span>
+              {/*Three Icon Bars in mobile displays*/}
+              <span className="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
+            </button>
+            <a href="./" className="navbar-brand"></a>
+          </div>
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div className="row">
+              <div className="col-md-push-2 col-md-8 col-lg-push-4 col-lg-6">
+                <ul className="nav navbar-nav">
+                  <li><Link to="/signup" className="col-sm-2 navlink">Signup</Link></li>
+                  <li><Link to="/login" className="col-sm-2 navlink">Login</Link></li>
+                </ul>{/* /nav narbar-nav */}
+              </div>
+            </div>
+            <div className="row">
+              {/* Notification bar */}
+            </div>{/* /row */}
+          </div>{/* /bs-example-navbar-collapse-1 */}
+        </div>{/* container */}
+      </nav>{/* navbar navbar-inverse navbar-fixed-top */}
     </div>
    )
  };
