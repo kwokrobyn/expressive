7773667780fb8ab0a018ff9ff3c2973a20805d1d
woodysee <woodyseesk@gmail.com>
2017-07-25 09:54:25 +0800

    Completed cool buttons for mobile

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -40,7 +40,7 @@ class Navbar extends Component {
             <div className="row">
               <div className="col-sm-7 col-md-8 col-lg-9"></div>
               <div className="col-sm-push-7 col-sm-5 col-md-push-8 col-md-4 col-lg-push-9 col-lg-3">
-                <ul className="nav navbar-nav">
+                <ul className="nav navbar-nav" id="navbar-list">
                   <li>
                     <button className="btn btn-success signup-btn">
                       <Link to="/signup" className="col-sm-2 navlink">
@@ -54,9 +54,6 @@ class Navbar extends Component {
                         Log in
                       </Link>
                     </button>
-                  </li>
-                  <li>
-                    <a href="http://freshdesign.agency/documents/presentations/FreshDesign-2017-presentation.pdf" class="btn btn-green">Download presentation</a>
                   </li>
                 </ul>{/* /nav narbar-nav */}
               </div>
