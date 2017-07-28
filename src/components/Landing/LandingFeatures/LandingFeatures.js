204fd81ad6790c946579819888f9f3155edb5ddb
Dionne Phua <dphua@ringling.edu>
2017-07-28 11:07:10 +0800

    merge conflicts resolved on join room

--- src/components/Landing/LandingFeatures/LandingFeatures.js
+++ src/components/Landing/LandingFeatures/LandingFeatures.js
@@ -1,90 +1,71 @@
 //Importing required packages
 import React, { Component, PropTypes } from 'react';
 import {Grid, Col, form, FormGroup, FormControl, ControlLabel, HelpBlock, Row, Button} from 'react-bootstrap';
-import { checkExisting } from '../../../actions/roomActions';
-import { connect } from 'react-redux';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 //Importing static assets (i.e. stylesheets, images)
 import './LandingFeatures.css';
 
 //Importing React Components
 
-/**
- * LandingAbout
- */
 
 class LandingFeatures extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-        value: ''
+        value: '',
+        joinroom: ""
         }
   }
 
 
-  // getValidationState() {
-  //   const length = this.state.value.length;
-  //   if (length > 10) return 'success';
-  //   else if (length > 5) return 'warning';
-  //   else if (length > 0) return 'error';
-  // }
-  //
-  // handleChange(e) {
-  //   this.setState({ value: e.target.value });
-  // }
-
-  checkExisting = (e) => {
-    e.preventDefault();
+  onChange = (e) => {
     let value = e.target.value;
     this.setState({joinroom: value});
-    // const roomString = document.getElementById('roomstring').value;
-    const joinroom = value;
-    console.log(joinroom);
-    this.props.checkExisting(joinroom);
-    //test commit
+    console.log(this.state.joinroom);
   }
 
   render() {
     return (
       <Grid>
 
-      {/* Join Room Input */}
-        <Row>
-          <Col md={10} mdOffset={4} sm={8} smOffset={3} xs={11} xsOffset={2} className="landingfeatures-join-rm">
-
-           <FormGroup
-             controlId="formBasicText"
-             >
+      {/* Join Room Url */}
+       <Row className="url-row">
+          <Col md={8} sm={8} smOffset={2} xs={10}  className="landingfeatures-join-rm">
+           <FormGroup>
              <ControlLabel className="joinRmLabel">Join A Room</ControlLabel>
              <FormControl
                type="text"
                placeholder="Enter Room URL"
                className="joinRmInput"
                id="joinroom"
-               onChange={this.checkExisting}
-               value={this.state.joinroom}
+               onChange={this.onChange}
+               value ={this.state.joinroom}
              />
-             <FormControl.Feedback />
-             {/*<HelpBlock>Validation is based on string length.</HelpBlock>*/}
-
-              <Button className="Join"}>Join</Button>
-
+            {/* Join Room Button */}
+              <Link to={"/room/" + this.state.joinroom}>
+                <div className="joinroom-button">Join Room</div>
+              </Link>
            </FormGroup>
            </Col>
         </Row>
 
         {/* How to Use Title */}
         <Row>
-          <Col md={8} mdOffset={3} sm={8} smOffset={3}>
-            <h1 className="how">How to use</h1>
+          <Col md={9} mdOffset={2} sm={10} smOffset={1} xs={9} xsOffset={1}>
+            <h1 className="shake shake-hard shake-constant--hover" id="how-title">How to use</h1>
           </Col>
         </Row>
 
         {/* Featurettes */}
         <Row>
-          <Col md={10} mdOffset={2} sm={10} smOffset={2} xs={10} xsOffset={1}>
+          <Col md={10} mdOffset={2} sm={10} smOffset={2} xs={10} xsOffset={1} className="featurettes">
           {/* make xs stack on top of each other when scaling responsively */}
             <Col md={4} xs={12} className="featureFrame01">
               <div className="features01">About</div>
