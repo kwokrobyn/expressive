e8ad1209eb2ad8c2fb1689b3f167d9853258f104
Dionne Phua <li.ann86@gmail.com>
2017-07-30 19:28:38 +0800

    Added footer image

--- src/components/Landing/LandingFooter/LandingFooter.js
+++ src/components/Landing/LandingFooter/LandingFooter.js
@@ -1,6 +1,6 @@
 //Importing required packages
 import React, { Component, PropTypes } from 'react';
-import {Row, Col} from 'react-bootstrap';
+import {Row, Col, Image} from 'react-bootstrap';
 
 //Importing static assets (i.e. stylesheets, images)
 import './LandingFooter.css';
@@ -22,11 +22,20 @@ import Footer from '../../Footer/Footer';
 
       {/* Mission Statement */}
         <Row>
-           <Col md={8} mdOffset={2} sm={8} smOffset={2} xs={8} xsOffset={2}>
-             <h2 id="landing-mission-title">Our Mission</h2>
-              <p id="landing-mission-paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+
+           <Col md={12} className="landing-footer-img">
+            {/*<Image src="../images/footerArt.png"/>*/}
+            <Col md={4} mdOffset={4} sm={4} smOffset={3} xs={6} xsOffset={3} className="landing-mission-title">
+            <h2 id="landing-mission-title">Our Mission</h2>
+            </Col>
+              <Col md={8} mdOffset={2} sm={8} smOffset={2} xs={8} xsOffset={2}>
+
+
+                 <p id="landing-mission-paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+              </Col>
            </Col>
         </Row>
+
 
         {/* Copyright */}
 
