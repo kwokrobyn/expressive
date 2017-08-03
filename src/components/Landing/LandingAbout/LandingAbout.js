08a849383ea961ebcb3d897aa57c03c9aacc383f
Dionne Phua <li.ann86@gmail.com>
2017-08-03 09:50:40 +0800

    About Carousel WIP

--- src/components/Landing/LandingAbout/LandingAbout.js
+++ src/components/Landing/LandingAbout/LandingAbout.js
@@ -32,6 +32,50 @@ class LandingAbout extends Component {
               <div className="aboutImg"></div>
             </Col>
 
+            <Col md={6} xs={12}>
+            <div id="myCarousel" className="carousel slide" data-ride="carousel">
+     {/* Indicators */}
+     <ol className="carousel-indicators">
+       <li data-target="#myCarousel" data-slide-to={0} className="active" />
+       <li data-target="#myCarousel" data-slide-to={1} />
+       <li data-target="#myCarousel" data-slide-to={2} />
+     </ol>
+     {/* Wrapper for slides */}
+     <div className="carousel-inner">
+       <div className="item active">
+         <img src="la.jpg" alt="Chania" />
+         <div className="carousel-caption">
+           <h3>Los Angeles</h3>
+           <p>LA is always so much fun!</p>
+         </div>
+       </div>
+       <div className="item">
+         <img src="chicago.jpg" alt="Chicago" />
+         <div className="carousel-caption">
+           <h3>Chicago</h3>
+           <p>Thank you, Chicago!</p>
+         </div>
+       </div>
+       <div className="item">
+         <img src="ny.jpg" alt="New York" />
+         <div className="carousel-caption">
+           <h3>New York</h3>
+           <p>We love the Big Apple!</p>
+         </div>
+       </div>
+     </div>
+     {/* Left and right controls */}
+     <a className="left carousel-control" href="#myCarousel" data-slide="prev">
+       <span className="glyphicon glyphicon-chevron-left" />
+       <span className="sr-only">Previous</span>
+     </a>
+     <a className="right carousel-control" href="#myCarousel" data-slide="next">
+       <span className="glyphicon glyphicon-chevron-right" />
+       <span className="sr-only">Next</span>
+     </a>
+   </div>
+            </Col>
+
             <Col md={6} xs={12} className="aboutText-box">
               <div className="aboutText">
                 <p>
